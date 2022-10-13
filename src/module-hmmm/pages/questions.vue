2b2e8a7fb7dd7cfea9391f441eb9f276770fe7e2
jr <1932933132@qq.com>
2022-10-13 08:39:02 +0800

    form表单功能完成

--- src/module-hmmm/pages/questions.vue
+++ src/module-hmmm/pages/questions.vue
@@ -1,9 +1,17 @@
 <template>
-  <div class='container'>题组管理</div>
+  <div class='container'>
+      <questions-public-page/>
+  </div>
 </template>
 
 <script>
-export default {}
+import questionsPublicPage from '../components/questions-public-page.vue'
+export default { components: { questionsPublicPage } }
 </script>
 
-<style scoped lang='less'></style>
+<style scoped lang='less'>
+.container{
+  padding: 0 10px;
+  margin: 10px 0;
+}
+</style>
