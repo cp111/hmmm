3790cbe5510c0370ae89377e6f7310e8a50365a0
lily <905355741@qq.com>
2022-10-16 11:23:52 +0800

    学科管理页面完成

--- src/module-hmmm/pages/subjects.vue
+++ src/module-hmmm/pages/subjects.vue
@@ -104,8 +104,8 @@
       label="操作"
       width="240">
       <template v-slot="{row}">
-          <span style="color: #409eff;  cursor:pointer;" @click="$router.push('directorys')">学科分类</span>
-          <span class="operate" @click="$router.push('tags')" >学科标签</span>
+          <span style="color: #409eff;  cursor:pointer;" @click="$router.push('directorys/'+ `?id=${row.id}&name=${row.subjectName}`)">学科分类</span>
+          <span class="operate" @click="$router.push('tags/' + row.id)" >学科标签</span>
           <span  class="operate" @click="EditSubject(row)">修改</span>
           <span  class="operate" @click="del(row)">删除</span>
       </template>
@@ -285,8 +285,9 @@ export default {
         this.getList()
       }
     },
-    // 修改学科
+    // 修改学科s
     EditSubject (row) {
+      console.log(row)
       this.editForm.id = row.id
       this.editForm.subjectName = row.subjectName
       this.editForm.isFrontDisplay = !!row.isFrontDisplay
